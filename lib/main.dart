edc5aef2099a20d38c662c50edf0d958a1a92ff4
Andrey Simakov <simakov1987@gmail.com>
2022-02-23 13:36:09 +0300

    Create ui step 1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_ui/constants.dart';
+import 'package:flutter_responsive_ui/screens/main/main_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,8 @@
+// import 'package:admin/sreens/dashboard/commponents/side_menu.dart';
+// import 'package:flutter/material.dart';
+
+// import '../../constants.dart';
+// import 'components/header.dart';
+// import 'components/my_files.dart';
+// import 'components/recent_files.dart';
+// import 'components/storage_details.dart';
